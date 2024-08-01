6740bd160cf08c8a05ed3d0a385573df76ee15f3
konstntokas <konstantin.ntokas@brockmann-consult.de>
2024-08-01 10:25:28 +0200

    README updated

--- examples/notebooks/stack_mode.py
+++
@@ -1,6 +0,0 @@
-from xcube.core.store import new_data_store, get_data_store_params_schema
-
-url = "http://127.0.0.1:8080/ogc"
-store = new_data_store("stac", url=url)
-data_ids = store.list_data_ids()
-print(data_ids)
