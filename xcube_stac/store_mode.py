f3dfa1d3dec02ef0e6f09c29974eede3a6be09ce
konstntokas <konstantin.ntokas@brockmann-consult.de>
2025-02-14 09:14:01 +0100

    filter items with wrong bbox test

--- xcube_stac/store_mode.py
+++ xcube_stac/store_mode.py
@@ -474,12 +474,10 @@ class StackStoreMode(SingleStoreMode):
             return None
 
         # delete items with wrong bbox in CDSE sentinel-2-l2a
-        import pdb
-
-        pdb.set_trace()
-        if self._searchable:
-            if self._catalog.url == CDSE_STAC_URL and data_id == "sentinel-2-l2a":
-                items = [item for item in items if item.bbox[2] - item.bbox[1] < 2]
+        print(len(items))
+        if CDSE_STAC_URL in self._url_mod and data_id == "sentinel-2-l2a":
+            items = [item for item in items if item.bbox[2] - item.bbox[1] < 2]
+        print(len(items))
         # group items by date
         grouped_items = groupby_solar_day(items)
 
